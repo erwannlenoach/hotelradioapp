f1d0686800cf65a4b1620b8d936996fdf1735dba
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-10-14 17:29:39 +0200

    Cover - fetched url

--- src/cover/index.js
+++ src/cover/index.js
@@ -1,29 +1,35 @@
-import React, {Component} from 'react';
-import { WebView } from 'react-native-webview';
+import React, {Component, useState, useEffect} from 'react';
+import { WebView, Image, Text } from 'react-native-webview';
+
+
+
+  
 
 
 const Cover = () => {
+  const [url, setUrl] = useState('https://hotelradioparis.com/wp-content/uploads/2021/09/241993163_245673940830326_7153954473531330161_n.jpg');
 
-  const jsCode = 
-  `
-  document.getElementsByClassName('elementor-section')[0].style.display = 'none'; 
-   `;
+  useEffect(() => {
+    console.log(url)
+   });
 
-   const displayCover = 
-   `
-   document.getElementsByClassName('proradio-customplayer')[0].style.display = 'block'; 
-    `;
-    setTimeout(() => {
-      this.webref.injectJavaScript(displayCover);
-    }, 3000);
+   async function myFetch() {
+  
+    const cheerio = require('react-native-cheerio');
+     
+    let response = await fetch('https://hotelradioparis.com/');
+    let text = await response.text();
+    const $ = cheerio.load(text);
+    let images = $.html('[class="proradio-slider__i wp-post-image"]')
+    let src = $(images).attr('src')
+  // setUrl(src)
 
-    
+  }
 
   return (
     <WebView
     source={{ uri: 'https://hotelradioparis.com/' }}
-   
-    injectedJavaScript={jsCode}
+
     ref={r => (webref = r)}
     onMessage={(event) => {}}
     mediaPlaybackRequiresUserAction={true}
